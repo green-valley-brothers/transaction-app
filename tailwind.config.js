2803b3671bc86b233adb9020ece5ee2f0f770f93
Kato Charles <kcharles52@gmail.com>
2024-02-27 15:49:33 +0300

    chore: setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        secondary: "#14171A",
+      },
+    }
+  },
+  plugins: [],
+}
+
